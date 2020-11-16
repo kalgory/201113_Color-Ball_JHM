ab40c653c51c9232fa85977dc7ef0e16f22a8c93
hyunmindev <jung@hyunmin.dev>
2020-11-16 17:46:23 +0900

    O(N*M)
    N = 공의 개수 M = 색 종류의 수

--- main.cpp
+++ main.cpp
@@ -7,26 +7,41 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-
-vector<int> getOutput(int number, vector<int>& color, vector<int>& size)
+struct Ball
 {
-  vector<int> output(number);
-  
+  int color;
+  int index;
+};
+
+vector<int> getOutput(int number, vector<int>& color_vec, vector<int>& size_vec)
+{
+  map<int, vector<Ball>> size_ball_vec_map;
   for(int i = 0; i < number; i++)
   {
-    for(int j = 0; j < number; j++)
+    size_ball_vec_map[size_vec[i]].push_back({color_vec[i], i});
+  }
+  
+  int sum = 0;
+  vector<int> output_vec(number);
+  map<int, int> color_size_map;
+  for(const auto& [size, ball_vec] : size_ball_vec_map)
+  {
+    for(const auto& ball : ball_vec)
     {
-      if (color[i] != color[j] && size[i] > size[j])
-      {
-        output[i] += size[j];
-      }
+      output_vec[ball.index] = sum - color_size_map[ball.color];
+    }
+    for(const auto& ball : ball_vec)
+    {
+      sum += size;
+      color_size_map[ball.color] += size;
     }
   }
   
-  return output;
+  return output_vec;
 }
 
 int main()
@@ -34,15 +49,21 @@ int main()
   int number;
   cin >> number;
   
-  vector<int> color(number);
-  vector<int> size(number);
+  vector<int> color_vec(number);
+  vector<int> size_vec(number);
   
   for(int i = 0; i < number; i++)
-    cin >> color[i] >> size[i];
+  {
+    scanf("%d %d", &color_vec[i], &size_vec[i]);
+//    cin >> color_vec[i] >> size_vec[i];
+  }
   
-  vector<int> output = getOutput(number, color, size);
+  vector<int> output_vec = getOutput(number, color_vec, size_vec);
   for(int i = 0; i < number; i++)
-    cout << output[i] << endl;
+  {
+    printf("%d\n", output_vec[i]);
+//    cout << output_vec[i] << endl;
+  }
   
   return 0;
 }
